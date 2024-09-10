a59cdcd49f4c2fb405c9003ea94e8262c70fc64a
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-09-10 20:08:17 +0530

    Added base project files

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,90 @@
-import Image from "next/image";
+import Image from 'next/image';
+import heroImage1 from './images/jasmin-chew-WKD2vIe8Rb0-unsplash.jpg';
+import heroImage2 from './images/kateryna-hliznitsova-EnvGlhMhFrc-unsplash.jpg';
+import heroImage3 from './images/kateryna-hliznitsova-ceSCZzjTReg-unsplash.jpg';
+import { MoveRight, Sparkles, User } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Badge } from '@/components/ui/badge';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	return (
+		<>
+			<main className='w-full py-20 lg:py-40 h-screen'>
+				<div className='container mx-auto'>
+					<div className='grid grid-cols-1 gap-8 items-center md:grid-cols-2'>
+						<div className='flex gap-12 flex-col'>
+							<div className='flex gap-8 flex-col'>
+								<h1 className='text-5xl md:text-7xl max-w-lg tracking-tighter text-left'>Timeless Elegance in Every Detail</h1>
+								<p className='text-xl leading-relaxed tracking-tight text-muted-foreground max-w-md text-left'>
+									Discover exquisite, handcrafted jewelry that embodies elegance and sophistication, designed to elevate your unique style.
+								</p>
+							</div>
+							<div className='flex flex-row gap-4'>
+								<Button size='lg' className='gap-4' variant='outline'>
+									<Sparkles className='w-4 h-4' /> Browse All
+								</Button>
+								<Button size='lg' className='gap-4' variant={'ghost'}>
+									Learn More <MoveRight className='w-4 h-4' />
+								</Button>
+							</div>
+						</div>
+						<div className='grid grid-cols-2 gap-8'>
+							<div className='aspect-square'>
+								<Image src={heroImage1} alt='hero image' className='rounded-md aspect-square object-cover' />
+							</div>
+							<div className='row-span-2 self-center'>
+								<Image src={heroImage2} alt='hero image' className='rounded-md object-cover' />
+							</div>
+							<div className='aspect-square'>
+								<Image src={heroImage3} alt='hero image' className='rounded-md aspect-square object-cover' />
+							</div>
+						</div>
+					</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+					<div className='flex flex-col gap-10 mt-32'>
+						<div className='flex gap-4 flex-col items-start'>
+							<div>
+								<Badge>New</Badge>
+							</div>
+							<div className='flex gap-2 flex-col'>
+								<h2 className='text-3xl md:text-5xl tracking-tighter max-w-xl text-left'>Latest Additions</h2>
+								<p className='text-lg max-w-xl lg:max-w-lg leading-relaxed tracking-tight text-muted-foreground text-left'>Up to date with the latest trends.</p>
+							</div>
+						</div>
+						<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
+							<div className='bg-muted rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col'>
+								<User className='w-8 h-8 stroke-1' />
+								<div className='flex flex-col'>
+									<h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+									<p className='text-muted-foreground max-w-xs text-base'>Our goal is to streamline SMB trade, making it easier and faster than ever.</p>
+								</div>
+							</div>
+							<div className='bg-muted rounded-md  aspect-square p-6 flex justify-between flex-col'>
+								<User className='w-8 h-8 stroke-1' />
+								<div className='flex flex-col'>
+									<h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+									<p className='text-muted-foreground max-w-xs text-base'>Our goal is to streamline SMB trade, making it easier and faster than ever.</p>
+								</div>
+							</div>
+
+							<div className='bg-muted rounded-md aspect-square p-6 flex justify-between flex-col'>
+								<User className='w-8 h-8 stroke-1' />
+								<div className='flex flex-col'>
+									<h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+									<p className='text-muted-foreground max-w-xs text-base'>Our goal is to streamline SMB trade, making it easier and faster than ever.</p>
+								</div>
+							</div>
+							<div className='bg-muted rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col'>
+								<User className='w-8 h-8 stroke-1' />
+								<div className='flex flex-col'>
+									<h3 className='text-xl tracking-tight'>Pay supplier invoices</h3>
+									<p className='text-muted-foreground max-w-xs text-base'>Our goal is to streamline SMB trade, making it easier and faster than ever.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</>
+	);
 }
