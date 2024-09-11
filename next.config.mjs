d540543e80cffecff25238b9571abb6f0fc10062
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-09-11 15:52:18 +0530

    Added unsplash api support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/*",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
