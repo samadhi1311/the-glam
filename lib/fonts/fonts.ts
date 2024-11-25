06821e7ac7c1ef64d0a4249be9cbab3f8ee599b5
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-25 19:29:05 +0530

    Added custom fonts

---
+++ lib/fonts/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Playfair_Display } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], display: 'swap', preload: true, variable: '--font-inter' });
+export const playfair = Playfair_Display({ subsets: ['latin'], display: 'swap', preload: true, weight: '400', variable: '--font-playfair' });
