71da9c2053a78a9d37c5f5d06b8f921b0f7b7167
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-25 19:29:56 +0530

    Added animations

---
+++ components/animations/animations.ts
@@ -0,0 +1,17 @@
+export const TextSwitch = {
+	initial: {
+		opacity: 0,
+	},
+	open: {
+		opacity: 1,
+		transition: {
+			duration: 0.2,
+		},
+	},
+	closed: {
+		opacity: 0,
+		transition: {
+			duration: 0.2,
+		},
+	},
+};
