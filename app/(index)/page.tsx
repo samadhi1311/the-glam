ab24959c0956829a9b97a25a56593c9363cfd614
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-25 19:30:58 +0530

    Added homepage

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,5 +1,16 @@
 'use client';
 
+import Footer from '@/components/footer';
+import Hero from '@/components/hero';
+import Products from '@/components/products';
+import { Page } from '@/components/ui/layouts';
+
 export default function Home() {
-	return <main></main>;
+	return (
+		<Page>
+			<Hero />
+			<Products />
+			<Footer />
+		</Page>
+	);
 }
