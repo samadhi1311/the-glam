c8a2a9a9d051b55888e535f93a1e2b5805e6bbc5
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-25 19:31:13 +0530

    Added Lenis

---
+++ app/(index)/layout.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import MenuButton from '@/components/ui/menu-button';
+import { ReactLenis } from '@studio-freight/react-lenis';
+import { motion } from 'motion/react';
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+	return (
+		<ReactLenis root options={{ lerp: 0.35 }}>
+			<MenuButton />
+
+			<motion.div>{children}</motion.div>
+		</ReactLenis>
+	);
+}
