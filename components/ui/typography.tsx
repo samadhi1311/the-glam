47228ccf642d14d327eb055e006148d1554f0987
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-27 23:50:58 +0530

    Updated typogrpahy components

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { cn } from '@/lib/utils/merge-classNames';
+import { cn } from '@/lib/utils';
 
 function H1({ className, children }: { className?: string; children?: React.ReactNode }) {
-	return <h1 className={cn('playfair text-4xl xs:text-6xl sm:text-7xl md:text-8xl lg:text-9xl uppercase', className)}>{children}</h1>;
+	return <h1 className={cn('playfair scale-y-95 tracking-widest text-4xl xs:text-6xl sm:text-7xl md:text-8xl lg:text-9xl uppercase', className)}>{children}</h1>;
 }
 
 function H2({ className, children }: { className?: string; children?: React.ReactNode }) {
-	return <h1 className={cn('playfair text-4xl md:text-5xl tracking-wide', className)}>{children}</h1>;
+	return <h1 className={cn('playfair scale-y-95 tracking-wide text-4xl md:text-5xl', className)}>{children}</h1>;
 }
 
 function H3({ className, children }: { className?: string; children?: React.ReactNode }) {
-	return <h2 className={cn('text-2xl md:text-3xl tracking-wide font-light', className)}>{children}</h2>;
+	return <h2 className={cn('text-2xl md:text-3xl tracking-tighter font-light', className)}>{children}</h2>;
 }
 
 function P1({ className, children }: { className?: string; children?: React.ReactNode }) {
