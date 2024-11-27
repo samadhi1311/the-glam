574e0759f735e86380016c5e368a53d9d0ca66f8
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-27 23:51:12 +0530

    Updated layout components

--- components/ui/layouts.tsx
+++ components/ui/layouts.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-function Page({ children }: Readonly<{ children?: React.ReactNode }>) {
-	return <div className='mx-auto'>{children}</div>;
+import { cn } from '@/lib/utils';
+
+function Page({ children, className }: Readonly<{ children?: React.ReactNode; className?: string }>) {
+	return <div className={cn('mx-auto min-h-svh', className)}>{children}</div>;
 }
 
-function Section({ children }: Readonly<{ children?: React.ReactNode }>) {
-	return <div className='mx-auto px-4 py-16 md:px-8 md:py-32'>{children}</div>;
+function Section({ children, className }: Readonly<{ children?: React.ReactNode; className?: string }>) {
+	return <div className={cn('mx-auto px-4 py-16 md:px-8 md:py-32', className)}>{children}</div>;
 }
 
 export { Page, Section };
