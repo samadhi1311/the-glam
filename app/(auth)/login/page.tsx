e8ca46b091a8b2ca78afac9199158608483d4ed0
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-01 18:52:44 +0530

    Updated Login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,16 +6,24 @@ import { Page } from '@/components/ui/layouts';
 import { PasswordInput } from '@/components/ui/password-input';
 import { Separator } from '@/components/ui/separator';
 import { H2, H3, P1, P2 } from '@/components/ui/typography';
+import { useAuth } from '@/lib/hooks/use-auth';
+import { LuLoader2, LuLogIn } from 'react-icons/lu';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Login() {
+	const { authLoading, login, user } = useAuth();
+
+	const [userCredentials, setUserCredentials] = useState({ email: '', password: '' });
+
 	return (
 		<Page className='flex h-full items-center justify-center'>
 			<div className='grid h-svh w-full grid-cols-1 md:grid-cols-2'>
 				<div className='relative hidden w-full overflow-hidden md:block'>
 					<span className='absolute left-4 top-4 z-10 md:left-8 md:top-8 lg:left-16 lg:top-16'>
-						<P1 className='leading-none'>Welcome to the, </P1>
+						<P1 className='leading-none'>Welcome back to, </P1>
 						<H2 className='scale-y-95'>THE GLAM</H2>
+						{user ? user.email : null}
 					</span>
 					<img src='/pawel-czerwinski-WEjv3BMP2ik-unsplash.jpg' alt='Background image' className='-z-10 object-cover opacity-50' />
 				</div>
@@ -27,11 +35,11 @@ export default function Login() {
 						</div>
 						<div className='mt-8 grid w-full gap-8'>
 							<div className='grid gap-2'>
-								<Input id='email' placeholder='Email' type='email' required />
+								<Input id='email' placeholder='Email' type='email' required onChange={(e) => setUserCredentials({ ...userCredentials, email: e.target.value })} />
 							</div>
 
 							<div className='grid gap-2'>
-								<PasswordInput id='password' placeholder='Password' type='password' required />
+								<PasswordInput id='password' placeholder='Password' type='password' required onChange={(e) => setUserCredentials({ ...userCredentials, password: e.target.value })} />
 								<div className='flex items-center'>
 									<Link href='#' className='ml-auto inline-block text-sm underline'>
 										Forgot your password?
@@ -40,19 +48,25 @@ export default function Login() {
 							</div>
 
 							<div className='mt-4 flex flex-col items-center justify-center gap-4'>
-								<Button type='submit' className='w-fit min-w-36'>
-									Login
-								</Button>
+								{authLoading ? (
+									<Button type='submit' className='flex w-fit min-w-36 items-center gap-3'>
+										<LuLoader2 className='size-4 animate-spin object-center' /> Wait
+									</Button>
+								) : (
+									<Button type='submit' className='flex w-fit min-w-36 items-center gap-3' onClick={() => login(userCredentials.email, userCredentials.password)}>
+										<LuLogIn className='size-4' /> Login
+									</Button>
+								)}
 								<span className='text-center text-sm'>or</span>
 								<Button variant='outline' className='w-fit min-w-36'>
-									Login with Google
+									Continue with Google
 								</Button>
 							</div>
 						</div>
 						<Separator />
 						<div className='text-center text-sm'>
 							Don&apos;t have an account?{' '}
-							<Link href='#' className='underline'>
+							<Link href='/signup' className='underline'>
 								Sign up
 							</Link>
 						</div>
